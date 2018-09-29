ec80c3056c9b973c32a7b8a7cfe7a36709903c3c
courtney persons <personscourtney@gmail.com>
2018-09-29 11:32:01 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,6 @@
 # Add  code here!
 def prime?(number)
-  prime?.each() do |number|
-  p prime  #=> 2, 3, 5, 7, 11, ...., 97
+  prime?(number).each do |number|
+  p number  #=> 2, 3, 5, 7, 11, ...., 97
 end
 end
